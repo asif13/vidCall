0adee9e18e597943c978b9855b1987dc424c5e4d
asif13 <asifjunaid7@gmail.com>
2017-01-14 19:50:54 +0400

    init

---
+++ VidCall/VidCall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VidCall
+//
+//  Created by Asif Junaid on 1/14/17.
+//  Copyright © 2017 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
